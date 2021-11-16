fba1a6b6c87a25855793398523c997d942e3fb74
alsoknownasdrew <andriy.frankevych@gmail.com>
2021-11-16 09:51:19 +0100

    add packages install script

---
+++ run_once_install-packages.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing Homebrew packages..."
+brew bundle --no-lock --file=/dev/stdin <<EOF
+tap "adoptopenjdk/openjdk"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+brew "chezmoi"
+brew "docker"
+brew "git"
+brew "git-lfs"
+brew "gradle"
+brew "helm"
+brew "kind"
+brew "kotlin"
+brew "neofetch"
+brew "node"
+cask "adoptopenjdk16"
+cask "alfred"
+cask "amethyst"
+cask "bitwarden"
+cask "discord"
+cask "firefox"
+cask "google-chrome"
+cask "intellij-idea"
+cask "iterm2"
+cask "lens"
+cask "libreoffice"
+cask "mongodb-compass"
+cask "notion"
+cask "obsidian"
+cask "postman"
+cask "slack"
+cask "spotify"
+cask "stats"
+cask "telegram"
+cask "visual-studio-code"
+cask "vlc"
+cask "zoom"
+EOF
+
+printf "TODO:\n\
+install: \n\
+Logi Options (https://www.logitech.com/en-us/product/options) \n\
+Logi Capture (https://www.logitech.com/en-us/product/capture) \n\
+\n\
+Log in everywhere!\n\
+"
